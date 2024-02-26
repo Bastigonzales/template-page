947e02390d31b67794984e9144146fdd201153be
Basti Gonzales <bastigonzales@protonmail.com>
2024-02-26 23:10:03 +0800

    Update main.js
    Added alert

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// FAQ Accordion
+document.addEventListener("DOMContentLoaded", () => {
+	const faqContainer = document.querySelector(".faq-content");
+
+	faqContainer.addEventListener("click", () => {
+		alert(1);
+	});
+});
