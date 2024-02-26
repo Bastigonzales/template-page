fc38934537f6d915318f8d407a7c79502ab743b0
Basti Gonzales <bastigonzales@protonmail.com>
2024-02-27 00:12:53 +0800

    plus and minus icon now moves when clicking

--- js/main.js
+++ js/main.js
@@ -2,7 +2,17 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const faqContainer = document.querySelector(".faq-content");
 
-	faqContainer.addEventListener("click", () => {
-		alert(1);
+	faqContainer.addEventListener("click", (e) => {
+		const groupHeader = e.target.closest(".faq-group-header");
+
+		if (!groupHeader) return;
+
+		const group = groupHeader.parentElement;
+		const groupBody = group.querySelector(".faq-group-body");
+		const icon = groupHeader.querySelector("i");
+
+		// Toggle icon
+		icon.classList.toggle("fa-plus");
+		icon.classList.toggle("fa-minus");
 	});
 });
